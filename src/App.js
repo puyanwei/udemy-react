b71077532c120070741ab3efeeaee51554bf54e8
Puyan Wei <puyanwei@gmail.com>
2018-06-18 17:17:22 +0100

    Added custom component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './components/Person';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <h1>Learning React through Udemy</h1>
+                <Person />
             </div>
         );
     }
---
+++ src/components/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Person = () => {
+    return <h1>This is a person</h1>;
+};
+
+export default Person;
