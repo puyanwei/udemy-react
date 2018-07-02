541b31b3cd9bd5c22a34195289e81ce13992c143
Puyan Wei <puyanwei@gmail.com>
2018-07-02 14:23:40 +0100

    Two way binding, dynamic changing output

--- src/App.js
+++ src/App.js
@@ -20,11 +20,23 @@ class App extends Component {
             ],
         });
     };
+    nameChangedHandler = (event) => {
+        this.setState({
+            characters: [
+                { name: 'Ryu', country: 'Japan' },
+                { name: 'Chun-Li', country: event.target.value },
+                { name: 'Ken', country: 'USA' },
+            ],
+        });
+    };
 
     render() {
         return (
             <div className="App">
                 <h1>Street Fighter</h1>
+                <button onClick={this.switchNameHandler.bind(this, 'E.Honda')}>
+                    Switch Name
+                </button>
                 <Person
                     name={this.state.characters[0].name}
                     country={this.state.characters[0].country}
@@ -39,10 +51,8 @@ class App extends Component {
                     name={this.state.characters[2].name}
                     country={this.state.characters[2].country}
                     click={this.switchNameHandler.bind(this, 'Guile')}
+                    changed={this.nameChangedHandler}
                 />
-                <button onClick={this.switchNameHandler.bind(this, 'E.Honda')}>
-                    Switch Name
-                </button>
             </div>
         );
     }
--- src/components/Person.js
+++ src/components/Person.js
@@ -7,6 +7,11 @@ const Person = (props) => {
                 I am {props.name} from {props.country}
             </p>
             <p>{props.children}</p>
+            <input
+                type="text"
+                onChange={props.changed}
+                placeholder="Chun-Li's country"
+            />
         </div>
     );
 };
